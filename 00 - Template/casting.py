77ccf3ed4ee64bcd0fb57bc276f998cb55dce457
hendar <hendar.online@gmail.com>
2021-10-31 21:19:40 +0700

    casting

---
+++ 00 - Template/casting.py
@@ -0,0 +1,10 @@
+# casting from data type to another
+
+data_int = 8
+print("data = ", data_int, "type = ", type(data_int))
+
+data_float = float(data_int)
+print("data = ", data_float, "type = ", type(data_float))
+
+data_str = str(data_int)
+print("data = ", data_str, "type = ", type(data_str))
